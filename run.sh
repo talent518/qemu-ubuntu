71007b95ec4f0aa11ab3c493432d343351cae075
阿宝 <talent518@yeah.net>
2021-06-11 17:24:39 +0800

    使用$platform变量作为文件或文件夹名的一部分。

--- run.sh
+++ run.sh
@@ -16,13 +16,14 @@ if [ "$platform" = "amd64" ]; then
 		kvm=kvm
 		pkg=qemu-kvm
 	else
-		kvm=qemu-system-$(uname -p)
+		kvm=qemu-system-x86_64
 		pkg=qemu-system-x86
 	fi
 
-	arch=$(uname -p)
+	arch=x86
 	cross=
 	image=bzImage
+	kconfig=x86_64_defconfig
 
 	append="root=/dev/sda rw console=ttyS0 loglevel=6 init=/bin/systemd $APPEND"
 elif [ "$platform" = "armhf" ]; then
@@ -49,33 +50,33 @@ fi
 
 sudo dpkg -l qemu-user-static $pkg
 
-if [ -f "bzImage-$arch" ]; then
-	kernel=./bzImage-$arch
+if [ -f "bzImage-$platform" ]; then
+	kernel=./bzImage-$platform
 else
-	kernel=linux-$kver-$arch/arch/$arch/boot/$image
+	kernel=linux-$kver-$platform/arch/$arch/boot/$image
 
-	if [ ! -d linux-$kver-$arch ]; then
+	if [ ! -d linux-$kver-$platform ]; then
 		if [ ! -f linux-$kver.tar.xz ]; then
 			wget -O linux-$kver.tar.xz https://cdn.kernel.org/pub/linux/kernel/v${kver:0:1}.x/linux-$kver.tar.xz
 		fi
 		tar -xvf linux-$kver.tar.xz
-		mv linux-$kver linux-$kver-$arch
+		mv linux-$kver linux-$kver-$platform
 	fi
 	
-	if [ ! -f linux-$kver-$arch/.config ]; then
-		sed 's|=m$|=y|g' linux-$kver-$arch/arch/$arch/configs/$kconfig > linux-$kver-$arch/arch/$arch/configs/qemu_defconfig
-		cat - >> linux-$kver-$arch/arch/$arch/configs/qemu_defconfig <<!
+	if [ ! -f linux-$kver-$platform/.config ]; then
+		sed 's|=m$|=y|g' linux-$kver-$platform/arch/$arch/configs/$kconfig > linux-$kver-$platform/arch/$arch/configs/qemu_defconfig
+		cat - >> linux-$kver-$platform/arch/$arch/configs/qemu_defconfig <<!
 CONFIG_BLK_DEV_RAM=y
 CONFIG_BLK_DEV_RAM_COUNT=16
 CONFIG_BLK_DEV_RAM_SIZE=65536
 !
-		make -C linux-$kver-$arch ARCH=$arch CROSS_COMPILE=$cross qemu_defconfig
+		make -C linux-$kver-$platform ARCH=$arch CROSS_COMPILE=$cross qemu_defconfig
 	fi
 
-	make -C linux-$kver-$arch ARCH=$arch CROSS_COMPILE=$cross -j$N
+	make -C linux-$kver-$platform ARCH=$arch CROSS_COMPILE=$cross -j$N
 fi
 
-if [ ! -f "boot-$arch.img" -o ! -f "boot-$arch.ok" ]; then
+if [ ! -f "boot-$platform.img" -o ! -f "boot-$platform.ok" ]; then
 	if [ ! -f ubuntu-base-$bver-base-$platform.tar.gz ]; then
 		wget -O ubuntu-base-$bver-base-$platform.tar.gz http://cdimage.ubuntu.com/ubuntu-base/releases/$bver/release/ubuntu-base-$bver-base-$platform.tar.gz
 	fi
@@ -88,10 +89,10 @@ if [ ! -f "boot-$arch.img" -o ! -f "boot-$arch.ok" ]; then
 		mkdir boot
 	fi
 
-	dd if=/dev/zero of=boot-$arch.img bs=1M count=8192
-	mkfs.ext4 -L rootfs boot-$arch.img
+	dd if=/dev/zero of=boot-$platform.img bs=1M count=8192
+	mkfs.ext4 -L rootfs boot-$platform.img
 
-	sudo mount boot-$arch.img boot
+	sudo mount boot-$platform.img boot
 	sudo tar -xvf ubuntu-base-$bver-base-$platform.tar.gz -C boot/
 
 	# mount: proc sys dev
@@ -117,7 +118,7 @@ else
 	apt install -y sudo language-pack-en-base ssh net-tools ethtool ifupdown iputils-ping htop vim kmod network-manager bind9-dnsutils sysstat make g++ gcc --fix-missing --no-install-recommends
 fi
 
-echo abao-u${bver:0:2}-$arch > /etc/hostname
+echo abao-u${bver:0:2}-$platform > /etc/hostname
 echo "abao ALL=(ALL:ALL) ALL" >> /dev/sudoers
 
 useradd -m -s /bin/bash -G adm,sudo abao
@@ -138,15 +139,15 @@ rm -vf /init
 
 	sudo umount boot
 
-	e2fsck -p -f boot-$arch.img
+	e2fsck -p -f boot-$platform.img
 	rmdir boot
 
-	touch boot-$arch.ok
+	touch boot-$platform.ok
 fi
 
 sudo $kvm -smp $N -m $msize \
 	-kernel $kernel \
-	-hda boot-$arch.img \
+	-hda boot-$platform.img \
 	-append "$append" \
 	-net nic,model=$nic \
 	-net tap,script=/etc/qemu-ifup,downscript=/etc/qemu-ifdown \
