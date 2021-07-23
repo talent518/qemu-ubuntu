bb6e412c171aa37222d95e304632c533a3f5247d
阿宝 <talent518@yeah.net>
2021-07-23 16:15:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ kver=${KVER:-5.12.7}
 bver=${BVER:-21.04}
 platform=${PLATFORM:-amd64}
 msize=${MSIZE:-1024M}
-sed=${SED:1}
 
 nic=e1000e
 kconfig=defconfig
@@ -134,7 +133,7 @@ if [ ! -f "boot-$platform.img" -o ! -f "boot-$platform.ok" ]; then
 	sudo mount -o bind /dev boot/dev
 	sudo mount -o bind /dev/pts boot/dev/pts
 
-	if [ $sed -ne 0 ]; then
+	if [ "x$SED" != "x0" ]; then
 		sudo sed -i 's|ports.ubuntu.com|mirrors.aliyun.com|g' boot/etc/apt/sources.list
 		sudo sed -i 's|archive.ubuntu.com|mirrors.aliyun.com|g' boot/etc/apt/sources.list
 		sudo sed -i 's|security.ubuntu.com|mirrors.aliyun.com|g' boot/etc/apt/sources.list
